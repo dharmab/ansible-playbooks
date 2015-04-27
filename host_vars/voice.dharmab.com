d293c8e9d3891f783f5d226c1e5a35e1bd0a7bb9
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2015-04-26 23:18:47 -0600

    use become directive (sudo is deprecated)

---
+++ host_vars/voice.dharmab.com
@@ -0,0 +1,3 @@
+murmur_motd: "Mumble server operated by Dharma Bellamkonda <dharma.bellamkonda@gmail.com>"
+murmur_password: ""
+murmur_superuser_password: "buNgFCU45Yc4"
---
+++ local.sh
@@ -0,0 +1,1 @@
+ansible-playbook -i localhost -c local --extra-vars "normal_user=dharmab user_real_name='Dharma Bellamkonda' user_email='dharma.bellamkonda@gmail.com' install_vmware=true" site.yml
